76009717d55d2e83dcbd0796701667652e4dbe16
Isinaveira <isinaveira@gmail.com>
2024-02-22 18:54:29 +0100

    Trying to resolve problems with PWA

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', pathMatch: 'full', component: LoginComponent},  
+];
