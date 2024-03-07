e5da6a7165b3edaa363c968511511cdf5b74612c
isinaveira <isaacmakey@gmail.com>
2024-03-07 20:45:05 +0100

    Users funciona

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 
-router.post('users', userController.createUser);                  // Crear un usuario
+router.post('/users', userController.createUser);                  // Crear un usuario
 router.get('/users', userController.getUsers);                     // Listar todas los usuarios de la BD
 router.delete('/users/:dni', userController.deleteUser);             // Metodo para borrar un usuario de la BD
 router.get('/users/:dni', userController.getUser);                   // Mostrar un usuario por su dni
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/Prueba', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/Prueba')
         .then(() => console.log('Conexión a MongoDB exitosa.'))
         .catch(err => console.error('Error al conectar a MongoDB:', err));
 
