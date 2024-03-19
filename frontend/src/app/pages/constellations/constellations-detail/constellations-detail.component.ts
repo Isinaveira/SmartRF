4af28585b0a40b66b98883e86220feacd35de3cf
angel-bl <bastoslopezangel@gmail.com>
2024-03-19 23:01:44 +0100

    Constellations form

--- frontend/src/app/pages/constellations/constellations-detail/constellations-detail.component.ts
+++ frontend/src/app/pages/constellations/constellations-detail/constellations-detail.component.ts
@@ -8,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrl: './constellations-detail.component.css'
 })
 export class ConstellationsDetailComponent {
+  freqInicial!: number 
+  freqFinal!: number
+  anchoDeCanal!: number
 
+  constructor(){
+
+  }
+  
+  onChange(event: any){    
+      const selectedValue = event.target.value;
+      const element = document.getElementById("advancedOptions");
+      if (element)
+        if (selectedValue === 'advanced') {
+          element.style.display = 'block';
+        } else {
+          element.style.display = 'none';
+        }
+  }
+  
+
+  validateForm() {
+    
+
+    if (this.freqFinal <= 25 || this.freqFinal >= 1764) {
+        alert('La frecuencia final debe estar entre 25 MHz y 1764 MHz.');
+        return false;
+    }
+
+    if (this.freqInicial >= this.freqFinal) {
+        alert('La frecuencia inicial debe ser menor que la frecuencia final.');
+        return false;
+    }
+
+    if (this.anchoDeCanal > (this.freqFinal - this.freqInicial)) {
+        alert('El ancho de canal debe ser menor o igual al ancho de banda total a evaluar.');
+        return false;
+    }
+
+    
+    console.log("Enviar a formulario")
+    return true;
+  }
 }
