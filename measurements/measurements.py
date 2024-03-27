15f6c87199405d6a2ca077cc3c8801cf07a2fa82
angel-bl <bastoslopezangel@gmail.com>
2024-03-27 22:29:07 +0100

    Modified measurement script

--- measurements/measurements.py
+++ measurements/measurements.py
@@ -1,50 +1,47 @@
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
 import SoapySDR
 from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CF32
-# import time
-# inicio = time.time()
-
 
 #################################################
 # INPUTS
-freqIni = 104e6 # Hz
-freqFin = 114e6
-t_capt = 0.25 # s
-noiseThreshold = -118 # dBm/Hz
-nfft = 1024 # 2048 4096 8192 ...
-mode = 'max' # 'avg' o 'max'
-chanBW = 250e3
+freqIni = 758e6 # Hz frcuencia inicial
+freqFinal = 768.77e6 # Hz frecuencia final
+t_capt = 0.25 # s tiempo de captura por cada estimación
+threshold = 'auto' # dBm/Hz umbral 'auto' para estimación adaptativa o valor para umbral constante
+nfft = 2048 # 2048 4096 8192 ... potencias de 2
+mode = 'avg' # 'avg' estima en función de potencia media o 'max' estima según valor máximo del espectro
+chanBW = 500e3 # Hz ancho de canal
 #################################################
 
+# Se estima el espectro utilizando ventanas deslizantes solapadas de 2.56 MHz, para posteriormente eliminar 280 kHz por cada extremo
+# de cada ventana para utilizar sólo la parte plana de la respuesta del filtro. Se utilizan nfft puntos del espectro para estimar cada 2,56 MHz.
+# Posteriormente se dividen los puntos estimados del espectro según chanBW.
 
-totalBW = freqFin - freqIni
-G = 42 # dB
-fs = 2.56e6 # samples/s
-filtBW = fs # Hz
-nWindows = int(np.ceil(totalBW/2.0e6))
+totalBW = freqFinal - freqIni # ancho de banda total
+G = 45 # dB ganancia del RTL
+fs = 2.56e6 # samples/s tasa de muestreo
+filtBW = fs # Hz ancho de filtro instantáneo
+nWindows = int(np.ceil(totalBW/2.0e6)) # número de ventanas en frecuencia de 2 MHz
 
-fc_first_win = freqIni + 1.0e6
+fc_first_win = freqIni + 1.0e6 # frecuencia central de la primera ventana
 
-# f_axis = np.arange(nfft)*fs/nfft - 0.5*fs
-# idx_limDown = np.where(f_axis == -1.0e6)[0][0] # -1.0 MHz index en f_axis
-# idx_limUp = np.where(f_axis == 1.0e6)[0][0] # 1.0 MHz index en f_axis
-
+# índices de la fft para recortar a 2 MHz
 idx_limDown = 112*nfft//1024
 idx_limUp = 912*nfft//1024
 
 nSpecPoints = idx_limUp - idx_limDown
-psd = np.empty(nWindows*nSpecPoints + 1)
+psd = np.empty(nWindows*nSpecPoints)
 
-nSamples = int(np.ceil(t_capt*fs))
+nSamples = int(np.ceil(t_capt*fs)) # número de muestras para estimación de 2.56 MHz
 
 sdr = SoapySDR.Device({'driver':'rtlsdr', "channel": "0"})
 
 rx_chan = 0
 sdr.setGainMode(SOAPY_SDR_RX, rx_chan, False)   # AGC desactivado
-sdr.setGain(SOAPY_SDR_RX, rx_chan, G) 
+sdr.setGain(SOAPY_SDR_RX, rx_chan, G) # configuración de ganancia
 
+# función para capturar datos del RTL
 def sdrCapture(sdr:SoapySDR.Device, rx_chan, fs, nSamples, filtBW, fc): 
     sdr.setFrequency(SOAPY_SDR_RX, rx_chan, fc) 
     sdr.setBandwidth(SoapySDR.SOAPY_SDR_RX,rx_chan,filtBW)
@@ -68,6 +65,7 @@ def sdrCapture(sdr:SoapySDR.Device, rx_chan, fs, nSamples, filtBW, fc):
     sdr.closeStream(rx_stream)
     return(samples)
 
+# estimamos densidad espectral de potencia por cada ventana de 2 MHz pxx y juntamos todas las estimaciones en psd
 for win in range(nWindows):
     fc = fc_first_win + win*2.0e6
     x = sdrCapture(sdr, rx_chan, fs, nSamples, filtBW, fc)
@@ -76,28 +74,26 @@ for win in range(nWindows):
     pxx = np.fft.fftshift(pxx)
     pxx[nfft//2] = 0 # eliminamos media
     psd[win*nSpecPoints:(win + 1)*nSpecPoints] = pxx[idx_limDown:idx_limUp]
-    if win == nWindows - 1:
-        psd[-1] = pxx[-1]
 
-nPointsPerChan = int(np.round(nfft*chanBW/fs))
-nChannels = int(len(psd)//nPointsPerChan)
+nPointsPerChan = int(np.round(nfft*chanBW/fs)) # número de puntos del espectro que le corresponden a chanBW
+nChannels =int((nfft*totalBW/fs)//nPointsPerChan) # número de canales
 
-results = np.zeros(nChannels, dtype=int)
+results = np.zeros(nChannels, dtype=int) # array que indica por cada canal 0 desocupado o 1 ocupado
+
+# comprobamos y estimamos umbral adaptativo
+if threshold == 'auto':
+    threshold = 10*np.log10(np.mean(np.sort(psd)[:int(0.2*len(psd))])) - G + 30
+
+# realizamos estimaciones de ocupación según mode y threshold para cada canal
 if mode == 'max':    
     for chan in range(nChannels):
         PdBm_Hz = 10*np.log10(np.max(psd[chan*nPointsPerChan:(chan + 1)*nPointsPerChan])) - G + 30
-        if PdBm_Hz > noiseThreshold:
+        if PdBm_Hz > threshold:
             results[chan] = 1
 else:
     for chan in range(nChannels):
         PdBm = 10*np.log10(np.sum(psd[chan*nPointsPerChan:(chan + 1)*nPointsPerChan])*fs/nfft) - G + 30
-        if PdBm > noiseThreshold + 10*np.log10(nPointsPerChan*fs/nfft):
+        if PdBm > threshold + 10*np.log10(nPointsPerChan*fs/nfft):
             results[chan] = 1
 
 print(results)
-
-# plt.plot(10*np.log10(psd)+30-G)
-# plt.savefig('psd13.png')
-
-# fin = time.time()
-# print(fin-inicio)
