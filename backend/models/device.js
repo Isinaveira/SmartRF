ad5f1ff30affb5d3da474d0b72ce1c3485695739
Kalsyl <lanubedediegoar@gmail.com>
2024-03-30 16:26:15 +0100

    Started to define types of messages

--- backend/models/device.js
+++ backend/models/device.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const deviceSchema = new mongoose.Schema({
-  coordinates:  {
-    lng: { type: Number, required: true},
-    lat: { type: Number, required: true}
+  coordinates: {
+    lng: { type: Number, required: true },
+    lat: { type: Number, required: true },
   },
-  state: { type: String, required: true},
-  last_lectureAt: {type: String, required: true}
+  state: { type: String, required: true },
+  last_lectureAt: { type: String, required: true },
 });
 
-const Device = mongoose.model('Device', deviceSchema, 'Devices');
+const Device = mongoose.model("Device", deviceSchema, "Devices");
 
 module.exports = Device;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  dni:  {type: String, required: true},
-  email:  {type: String, required: true},
+  dni: { type: String, required: true },
+  email: { type: String, required: true },
   role: { type: String, required: true },
-  department:  {type: String, required: true},
+  department: { type: String, required: true },
   password: { type: String, required: true },
-  timeStamp: {type: Date, default: Date.now()}
+  timeStamp: { type: Date, default: Date.now() },
 });
 
-const User = mongoose.model('User', userSchema, 'Users');
+const User = mongoose.model("User", userSchema, "Users");
 
 module.exports = User;
