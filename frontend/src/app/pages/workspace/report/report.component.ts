f5bde30d9c44fd2f97de2cb2f3785f202dfb08c7
lucasvilarinho7 <lucas2002.vg@gmail.com>
2024-04-17 17:12:53 +0200

    starting report

---
+++ frontend/src/app/pages/workspace/report/report.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { UsersService } from '@/services/users.service';
+import { CookieService } from 'ngx-cookie-service';
+import { formatDate } from '@angular/common';
+import { Console } from 'console';
+
+
+@Component({
+  selector: 'app-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './report.component.html',
+  styleUrl: './report.component.css'
+})
+export class ReportComponent {
+
+  currentDate!: string;
+  name!: string;
+  dni!: string;
+
+  constructor(private userService : UsersService, private cookieService: CookieService) {
+
+    this.dni = this.cookieService.get('dniCookie');
+
+    const fechaActual = new Date();
+
+
+    this.currentDate = formatDate(fechaActual, 'dd/MM/yyyy', 'en-US');
+    this.userService.getUser(this.dni).subscribe({
+      next: (data) => { 
+        console.log(data);
+        this.name = data.name;
+       
+      }
+});
+    
+
+  }
+
+
+  
+
+}
--- frontend/src/app/pages/workspace/workspace.component.ts
+++ frontend/src/app/pages/workspace/workspace.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { ReportComponent } from './report/report.component';
+import  jsPDF from 'jspdf';
+import  html2canvas from 'html2canvas';
+import { NavbarComponent } from '@/components/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [ReportComponent, NavbarComponent],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.css'
 })
 export class WorkspaceComponent {
+
+  public isActive: boolean = false;
+
+  constructor(){}
+
+  visualizeReport(){
+
+    this.isActive = true;
+  }
+
+  cancel(){
+
+    this.isActive = false;
+  }
+
+  generateReport(){
+
+    const DATA = document.getElementById('report');
+    // Crear un nuevo documento PDF
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    
+    html2canvas(DATA!, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+      
+      // Añadir imagen Canvas a PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+      }).then((docResult) => {
+      docResult.save('informe.pdf');
+      });
+
+
+
+  }
 
 }
